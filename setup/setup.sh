e133fcaf394bca86741054cb30b4326290854f5b
Kat <kat@macbook>
2015-08-06 16:52:25 +0100

    bootstrap for ec2

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 #update all
-sudo apt-get update
-sudo apt-get clean
-
-#install java
-sudo apt-get install -y openjdk-7-jdk
+yum update -y
+yum clean all
 
 #install maven
-sudo apt-get install -y wget
 wget http://apache.mirrors.ionfish.org/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
 tar xzf apache-maven-3.3.3-bin.tar.gz -C /usr/local
 ln -s /usr/local/apache-maven-3.3.3 /usr/local/maven
 ln -s /usr/local/maven/bin/mvn /bin/mvn
 
 #install git
-sudo apt-get install git
+yum install -y git
+
+#clone the repository
+cd /home/ec2-user/
+git clone https://bitbucket.org/kizzie/qadockerint-exercises.git
+git clone https://bitbucket.org/kizzie/hello-scalatra.git
+
