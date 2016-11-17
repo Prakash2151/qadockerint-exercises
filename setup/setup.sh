e16e6e86466eaf757eda81330e2c7cb437db8242
Kat <kat.samperi@gmail.com>
2016-11-17 15:34:18 +0100

    updated setup file. Its not used from this repo anymore - all in cf template

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,9 +5,9 @@ yum update -y
 yum clean all
 
 #install maven
-wget http://apache.mirrors.ionfish.org/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
-tar xzf apache-maven-3.3.3-bin.tar.gz -C /usr/local
-ln -s /usr/local/apache-maven-3.3.3 /usr/local/maven
+wget http://apache.mirrors.ionfish.org/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar xzf apache-maven-3.3.9-bin.tar.gz -C /usr/local
+ln -s /usr/local/apache-maven-3.3.9 /usr/local/maven
 ln -s /usr/local/maven/bin/mvn /bin/mvn
 
 #install git
@@ -15,12 +15,9 @@ yum install -y git
 
 #clone the repository
 cd /home/ec2-user/
-git clone https://bitbucket.org/kizzie/qadockerint-exercises.git
-git clone https://bitbucket.org/kizzie/hello-scalatra.git
-git clone https://bitbucket.org/kizzie/qadockerint-eventsproject.git
+git clone https://gitlab.com/qatraining/qadockerint-exercises
+git clone https://gitlab.com/qatraining/hello-scalatra
+git clone https://gitlab.com/qatraining/qadockerint-eventsproject
 chown -R ec2-user:ec2-user qadockerint-exercises
 chown -R ec2-user:ec2-user hello-scalatra
 chown -R ec2-user:ec2-user qadockerint-eventsproject
-
-#create the docker nuke alias (may not be working properly)
-alias dockernuke='docker kill $(docker ps -a -q) && docker rm $(docker ps -a -q)'
